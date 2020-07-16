684d62168ed5753b1de0829f6b1546fe0014c406
avashist <avashist98@tamu.edu>
2020-07-15 22:13:49 -0500

    Need to be tested

--- StringArrays/RotateMatrix.java
+++ StringArrays/RotateMatrix.java
@@ -1,43 +1,46 @@
 public class RotateMatrix {
-    static void transpose(int arr[][]){
-        int N = arr.length;   
-        for (int i = 0; i < N; ++i){
-            for (int j = 0; j <i+1; ++j)
-            {
-                int temp = arr[i][j];
+    static void transpose(final int arr[][]) {
+        final int N = arr.length;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < i + 1; ++j) {
+                final int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
             }
         }
     }
-    static void flipColumns(int arr[][]){
-        for (int j = 0; j < N; ++j){
-            for(int i = 0; i < N/2; ++i){
-                int temp = arr[i][j];
-                arr[i][j] = arr[N-i][j];
-                arr[N-i][j] = temp;
+
+    static void flipColumns(final int arr[][]) {
+        for (int j = 0; j < N; ++j) {
+            for (int i = 0; i < N / 2; ++i) {
+                final int temp = arr[i][j];
+                arr[i][j] = arr[N - i][j];
+                arr[N - i][j] = temp;
             }
 
         }
 
     }
-    static int[][] rotateMatrix(int[][] Matrix) throws Exception{
-        int row = Matrix.length;
-        int col = Matrix[0].length;
-        if (row != col) throw new Exception("The Matrix is not a square");
+
+    static int[][] rotateMatrix(final int[][] Matrix) throws Exception {
+        final int row = Matrix.length;
+        final int col = Matrix[0].length;
+        if (row != col)
+            throw new Exception("The Matrix is not a square");
         transpose(Matrix);
         flipColumns(Matrix);
         return Matrix;
     }
-    static void printMatrix(int arr[][]) { 
-        for (int i = 0; i < arr.length; i++) { 
-            for (int j = 0; j < arr[0].length; 
-                 j++) 
-                System.out.print(arr[i][j] + " "); 
-            System.out.println(""); 
-        } 
-    } 
-    public static void main(String[] args) {
+
+    static void printMatrix(final int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println("");
+        }
+    }
+
+    public static void main(final String[] args) {
         int arr[][] = { { 1, 2, 3, 4 }, 
                         { 5, 6, 7, 8 }, 
                         { 9, 10, 11, 12 }, 
