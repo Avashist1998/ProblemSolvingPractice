6665682f6507584bea165633267ca456a4bd25fc
avashist1998 <avashist98@tamu.edu>
2021-06-24 04:59:44 -0500

    Update from Arch

--- StackAndQueues/arrayStack.py
+++ StackAndQueues/arrayStack.py
@@ -1,6 +1,31 @@
-from StackAndQueues import stack  
-from StackAndQueues import queue
+class nStack():
+    def __init__(self, n:int, size:int):
+        total_lenght = n*size
+        self.n = n
+        self.sizes = [0]*n
+        self.size_each = size
+        self.values = [None]*total_lenght
 
-que = queue()
-que.add_item(2)
-print(que.peek())
+    def push(self, stack_type:int, val:int):
+        size = self.sizes[stack_type]
+        if (size == self.size_each):
+            raise ValueError("Stack is overloaded")
+        else:
+            index = stack_type*self.size_each + self.sizes[stack_type]
+            self.values[index] = val
+            self.size[stack_type] = size + 1
+    
+    def pop(self, stack_type:int):
+        index = stack_type*self.size_each + self.sizes[stack_type] - 1
+        val = self.values[index]
+        self.values[index] = None
+        self.sizes[stack_type]+=-1
+        return val
+
+    def top(self, stack_type:int):
+        index = stack_type*self.size_each + self.sizes[stack_type] - 1
+        val = self.values[index]
+        return val
+        
+    def is_empty(self, stack_type:int):
+        return self.sizes[stack_type] == 0
