4b8aff175deaa8d9666aa4999c97e3a95db9c7ba
avashist1998 <avashist98@tamu.edu>
2021-11-02 06:47:52 -0500

    fix: isPathBetweenNOdes update

--- Graphs/isPathBetweenNodes.py
+++ Graphs/isPathBetweenNodes.py
@@ -1,11 +1,10 @@
 from .Node import Node
 
-
-
 def isPathBetweenNodes(start:Node, end:Node):
 
     # Beadth First Search 
-    
+    # Assume start and end can not be none 
+
     status = dict()
     queue = list()
     status[start.val] = "visiting"
@@ -15,18 +14,19 @@ def isPathBetweenNodes(start:Node, end:Node):
     while(len(queue) != 0):
 
         current_node = queue.pop(0)
-        if current_node != None:
-            if status[current_node.val] != 'visited':
-                if current_node == end:
-                    return True 
+            
+        if status[current_node.val] != 'visited':
+            if current_node == end:
+                return True 
 
-                else:
-                    for neighbor in current_node.neighbors:
+            else:
+                for neighbor in current_node.neighbors:
+                    if neighbor:
                         status[neighbor.val] = 'visiting'
                         queue.append(neighbor)
 
-        
-            status[current_node.val] = "visited"
+    
+        status[current_node.val] = "visited"
 
     
     return False 
